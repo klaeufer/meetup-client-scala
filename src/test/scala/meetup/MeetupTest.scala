8ac9b5205d2a60a185abaccd50f218b19a01e3ad
Konstantin Läufer <klaeufer@gmail.com>
2017-12-04 17:24:34 -0600

    added simple unit test
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- src/test/scala/meetup/MeetupTest.scala
+++ src/test/scala/meetup/MeetupTest.scala
@@ -1,12 +1,19 @@
 package edu.luc.etl.connectorspace.meetup
 
+import com.github.nscala_time.time.Imports._
 import org.specs2.mutable._
 
 class MeetupTest extends Specification {
 
-  "This unit test" should {
-    "just run" in {
-      true
+  "An Effort instance" should {
+    "return its attributes" in {
+      val fromDate = DateTime.lastMonth
+      val toDate = DateTime.now
+      val duration = 5.hours
+      val fixture = Effort(fromDate, toDate, duration)
+      (fixture.from must be equalTo (fromDate)) and
+        (fixture.to must be equalTo (toDate)) and
+        (fixture.duration must be equalTo (duration))
     }
   }
 }
